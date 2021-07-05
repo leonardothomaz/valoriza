7340d659fb3f234bbe98e7d26615971657462b02
Leonardo Thomaz <leonardo.thomaz@gmail.com>
2021-07-04 21:14:12 -0300

    feat(Aula 3 NLW): Criada rota de cadastro de Tag, feita tratativa de erros, e criado um middleware fake temporario ate que seja implementada autenticação

---
+++ src/repositories/TagsRepositories.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Tag } from '../models/Tag';
+
+@EntityRepository(Tag)
+class TagsRepository extends Repository<Tag> {}
+
+export { TagsRepository };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import './database';
 import { router } from './routes';
@@ -8,6 +9,21 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof Error) {
+            return response.status(400).json({
+                error: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log('servidor iniciado na porta 3333!');
